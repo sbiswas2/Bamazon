8dde7c0ebdc22deb33ba0b335f8d915b0599ceac
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-10-20 20:53:47 -0500

    finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+
 // create connection to sql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -12,13 +13,14 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+
 // connect to mysql server and sql database
 connection.connect(function(error) {
   if (error) throw error;
   // call info function to show products
   allInfo();
-  
 });
+
 
 // display item_id, product_name, price
 function allInfo() {
@@ -32,6 +34,7 @@ function allInfo() {
     promptCustomer();
   });
 };
+
 
 // function to prompt user of purchase
 function promptCustomer() {
@@ -54,25 +57,28 @@ function promptCustomer() {
     	connection.query("SELECT * FROM products WHERE item_id = " + id + ";",
     	function(error, results) {
     		if (error) throw error;
+    		// calculations
     		var quantity = parseInt(results[0].stock_quantity);
     		var price = parseInt(results[0].price);
     		var cost = units * price;
     		var remaining = quantity - units;
     		if (quantity === 0 || remaining < 0) {
-    			noInventory();
+    			noInventory(); // re-direct to function for no inventory
     		} else {
-    			console.log("Total Cost: $" + cost + " Dollars");
-    			updateInventory(id, units, quantity);
+    			console.log("Total Cost: $" + cost + " Dollars"); // display total cost
+    			updateInventory(id, units, quantity); // re-direct to function to update
     		};
 		});
     });
 };
 
+
 // function if quantity is 0
 function noInventory() {
 	console.log("Insufficient quantity!");
-	end();
+	end(); // end
 };
+
 
 // function to update quantity
 function updateInventory(id, units, quantity) {
@@ -91,8 +97,9 @@ function updateInventory(id, units, quantity) {
               if (error) throw err;
             }
           );
-	end();
+	end(); // end
 };
+
 
 // function to stop connection and return
 function end() {
