4e8421e9378b0b95c86bd37b4952466b35f88996
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-10-21 20:04:31 -0500

    pseudocoded functions needed for manager task

---
+++ bamazonManager.js
@@ -0,0 +1,87 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+// create connection to sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  // username
+  user: "root",
+  // password
+  password: "",
+  database: "bamazon"
+});
+
+
+// connect to mysql server and sql database
+connection.connect(function(error) {
+  if (error) throw error;
+  // call prompt function to ask manager
+  promptManager();
+});
+
+
+function promptManager() {
+    inquirer
+    .prompt({
+      name: "filter",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.filter) {
+        case "View Products for Sale":
+          products();
+          break;
+
+        case "View Low Inventory":
+          lowInventory();
+          break;
+
+        case "Add to Inventory":
+          addInventory();
+          break;
+
+        case "Add New Product":
+          newProduct();
+          break;
+      }
+    });
+};
+
+
+function products() {
+    console.log("view products");
+    end();
+};
+
+
+function lowInventory() {
+    console.log("low inventory");
+    end();
+};
+
+
+function addInventory() {
+    console.log("add inventory");
+    end();
+};
+
+
+function newProduct() {
+    console.log("new products");
+    end();
+};
+
+
+// function to stop connection and return
+function end() {
+    connection.end();
+};
